657c889d656ad385010e725b885a25ee63c93ed8
beecodeguy <beecodeguy@gmail.com>
2024-09-25 13:05:41 +0545

    fixed

--- components/shared/hero-section.tsx
+++ components/shared/hero-section.tsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import { getHeroDetails } from "@/actions/homepage";
+import CustomMDReactComponent from "@/provider/CustomMdComponent";
+import React from "react";
+import { Button } from "../ui/button";
 
-const HeroSection: React.FC = () => {
+const HeroSection: React.FC = async () => {
+  const heroDetails = await getHeroDetails();
+
+  const { title, image, tag } = heroDetails;
+
   return (
     <section
       className="relative h-screen bg-cover bg-center"
@@ -10,28 +17,25 @@ const HeroSection: React.FC = () => {
     >
       <div className="absolute inset-0 bg-black bg-opacity-60"></div>
       <div className="relative z-10 flex flex-col items-start justify-center h-full text-white px-6 md:px-20 lg:px-32">
-        <h1 className="text-lg md:text-6xl font-extrabold mb-4 leading-tight tracking-wide">
-          Empowering <span className="text-red-500">Ideas</span>,
-        </h1><br/>
-        <h1 className="text-lg md:text-6xl font-extrabold mb-4 leading-tight tracking-wide">
-        Igniting Change  
-        </h1>
-        <p className="md:text-2xl mb-8 font-semibold">
-          Nepal<span className="text-red-500">'</span>s Biggest <span className="text-red-500">TEDx</span> Event Happening Soon.
-        </p>
-
-       <br />
+        <CustomMDReactComponent
+          className="hero_title text-lg md:text-6xl font-extrabold mb-4 leading-tight tracking-wide"
+          text={title}
+        />
+        <CustomMDReactComponent
+          className="hero_tag md:text-2xl mb-8 font-semibold"
+          text={tag}
+        />
         <div className="flex space-x-4">
-          <button className="bg-red-500 hover:bg-red-c1 text-white font-bold py-3 px-20 rounded-md shadow-lg text-xl">
+          <Button variant="destructive" className="font-bold text-xl py-8 px-6">
             Explore More
-          </button>
-          <button className="bg-white hover:bg-gray-200 text-black-c1 font-bold py-3 px-20 rounded-md shadow-lg text-xl">
+          </Button>
+          <Button className="font-bold text-xl py-8 px-6" variant="secondary">
             Buy Tickets
-          </button>
+          </Button>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
