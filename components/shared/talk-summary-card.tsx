9962e951e845c97ef4ba1516dcdb012d643890a0
Prashanna21 <Prashnnanewar21@gmail.com>
2024-10-11 12:13:11 +0545

    talk-section responsive

--- components/shared/talk-summary-card.tsx
+++ components/shared/talk-summary-card.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const TalkSummaryCard = ({ src, title }: TTalkSummaryCard) => {
   return (
-    <div className="flex gap-6 justify-start items-start">
+    <div className="flex flex-col md:flex-row md:gap-6 justify-start items-start">
       <div className="relative min-w-[210px] min-h-[145px]">
         <div className="absolute top-2 left-2">
           <Image
@@ -19,7 +19,7 @@ const TalkSummaryCard = ({ src, title }: TTalkSummaryCard) => {
           height={145}
           alt="speaker"
           src={src}
-          className="h-[145px]"
+          className="h-[200px] w-96 md:h-[145px] md:w-[210px]"
         />
       </div>
       <p className="font-medium text-lg text-justify line-clamp-5">{title}</p>
