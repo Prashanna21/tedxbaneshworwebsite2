78b7646bc279874cb6b77809713cb0258a11e5a6
beecodeguy <beecodeguy@gmail.com>
2024-10-23 13:03:06 +0545

    about us page fixes

--- app/(public)/about-us/page.tsx
+++ app/(public)/about-us/page.tsx
@@ -1,20 +1,28 @@
-import AboutBaneshwor from '@/components/shared/AboutBaneshwor'
-import AboutExplore from '@/components/shared/AboutExplore'
-import AboutHero from '@/components/shared/aboutHero'
-import AboutMission from '@/components/shared/AboutMission'
-import AboutTedx from '@/components/shared/AboutTedx'
-import React from 'react'
+import AboutBaneshwor from "@/components/shared/AboutBaneshwor";
+import AboutExplore from "@/components/shared/AboutExplore";
+import AboutHero from "@/components/shared/AboutHero";
+import AboutMission from "@/components/shared/AboutMission";
+import AboutTedx from "@/components/shared/AboutTedx";
+import React from "react";
 
-const page = () => {
+const AboutRoute = () => {
   return (
-    <div className='p-1'>
+    <div className="p-1">
       <AboutHero />
-      <div className='bg-white p-20'><AboutTedx /></div>
-      <div className='bg-slate-300'><AboutBaneshwor /></div>
-      <div className='bg-white p-20'><AboutMission /></div>
-      <div className='bg-slate-300'><AboutExplore /></div>
+      <div className="bg-white p-20">
+        <AboutTedx />
+      </div>
+      <div className="bg-slate-300">
+        <AboutBaneshwor />
+      </div>
+      <div className="bg-white p-20">
+        <AboutMission />
+      </div>
+      <div className="bg-slate-300">
+        <AboutExplore />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default AboutRoute;
--- constants/example.ts
+++ constants/example.ts
@@ -90,7 +90,7 @@ export const FOOTER_CONTACT_INFO_EXAMPLE = {
 
 export const NAV_LINKS_EXAMPLE = [
   { id: "1", url: "/", title: "Home" },
-  { id: "2", url: "/", title: "About" },
+  { id: "2", url: "/about-us", title: "About" },
   { id: "3", url: "/", title: "Explore" },
   { id: "4", url: "/", title: "Contact" },
 ];
