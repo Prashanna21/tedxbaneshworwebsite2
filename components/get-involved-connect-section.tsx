0d0781f5acf5a53d096f10686673de5fe7689c6b
Prashanna21 <Prashnnanewar21@gmail.com>
2024-11-29 18:00:14 +0545

    get involved connect section text changed

--- components/get-involved-connect-section.tsx
+++ components/get-involved-connect-section.tsx
@@ -5,7 +5,7 @@ const GetInvolvedConnectSection = () => {
         <section className="bg-red-500 text-white flex flex-col justify-center items-center min-h-[330px]">
             <div className="flex flex-col justify-center items-center max-w-[1218px] gap-4">
                 <h2 className="text-3xl md:text-5xl font-bold">Connect with us</h2>
-                <p className="text-center text-sm">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam fugit, provident consectetur voluptatibus, eaque maxime saepe minima pariatur recusandae fuga dolores non dolorem? Eos nemo, accusantium repudiandae corrupti dolor hic! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem doloremque autem architecto fuga nulla atquelestiae magnam aliquid, voluptatum rerum mollitia vel quasi.</p>
+                <p className="text-center text-sm">We believe in the power of ideas to transform lives, and we want you to be a part of this incredible journey. TEDx Baneshwor’s 2nd Edition is around the corner, and we’re calling upon passionate individuals to contribute their expertise and talent. Whether you're looking to inspire as a <strong>Speaker</strong>, assist as a <strong>Volunteer</strong>, or perform as a <strong>Performer</strong>, there’s a place for you!</p>
                 </div>
         </section>
     )
